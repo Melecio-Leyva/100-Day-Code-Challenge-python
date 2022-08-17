13c7bd3ea91b516ec31500d0305534e4ba2c3ca3
Mele <61437633+Melecio-Leyva@users.noreply.github.com>
2022-08-16 23:13:52 -0700

    Day 1 coding project challenge
    Create a random name from user input.

---
+++ Day_1_python_project.py
@@ -0,0 +1,4 @@
+print("Hello Welcome to the randome name Generator")
+city= input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your new name is "+ city + " " + pet + "'s")
